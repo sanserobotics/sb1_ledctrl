655763ee220c9a4cee4276d2fb64def2d5f0d1c8
ubuntu <tjpink@gmail.com>
2024-07-20 17:32:41 +0800

    ledctrl serviec on/off working

--- ledctrl/ledctrl_service.py
+++ ledctrl/ledctrl_service.py
@@ -1,7 +1,11 @@
 from custom_interfaces.srv import Ledctrl
 import rclpy
 from rclpy.node import Node
+import RPi.GPIO as GPIO
+import time
 
+LED1 = 19
+LED2 = 26
 
 class MinimalService(Node):
 
@@ -14,8 +18,10 @@ class MinimalService(Node):
         response.led2_status = request.led2
         if request.led1 == 0:
             led1_req = "OFF"
+            GPIO.output(LED1, False)
         elif request.led1 == 1:
             led1_req = "ON"
+            GPIO.output(LED1, True)
         elif request.led1 == 2:
             led1_req = "Blink"
         else:
@@ -23,8 +29,10 @@ class MinimalService(Node):
        
         if request.led2 == 0:
             led2_req = "OFF"
+            GPIO.output(LED2, False)
         elif request.led2 == 1:
             led2_req = "ON"
+            GPIO.output(LED2, True) 
         elif request.led2 == 2:
             led2_req = "Blink"
         else:
@@ -36,6 +44,12 @@ class MinimalService(Node):
 
 
 def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED1, GPIO.OUT)
+    GPIO.setup(LED2, GPIO.OUT)
+    GPIO.output(LED1, False)
+    GPIO.output(LED2, False)
+
     rclpy.init()
 
     minimal_service = MinimalService()
